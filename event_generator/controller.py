e41c3105bee849e8feb77575959f93ed61962830
Muhammad Adnan <your_email@example.com>
2026-08-02 12:18:35 +0500

    Implemented random Account Collection Generation

--- event_generator/controller.py
+++ event_generator/controller.py
@@ -1,40 +1,9 @@
-# import time
-# from event_generator.generators.customer_generator import generate_customer 
-# from event_generator.generators.account_generator import generate_account
-# from event_generator.generators.merchant_generator import generate_merchant
-# from event_generator.generators.transaction_generator import generate_transaction
-# from event_generator.formatter import format_event
 
-# def run_simulation():
-#     customer=generate_customer()
-#     account=generate_account(customer.customer_id)
-#     merchant=generate_merchant() 
-#     transaction=generate_transaction(source_account_id=account.account_id,merchant_id=merchant.merchant_id)
-
-#     return{
-#         "customer":customer,
-#         "account":account,
-#         "merchant":merchant,
-#         "transaction":transaction
-#     }
-
-
-# # def run_event_generator():
-# #     while True:
-# #         event=run_simulation()
-# #         print(event["customer"])
-# #         print(event["account"])
-# #         print(event["merchant"])
-# #         print(event["transaction"])
-# #         print("-" * 80)
-# #         time.sleep(1)
-      
-# def run_event_generator():
-#     while True:
-#         event = run_simulation()
-#         print(format_event(event))
-#         time.sleep(1)
-
+# ==========================================================
+# Customer Collection Generation
+# ==========================================================
+# Generates the master list of banking customers.
+# Each customer is created independently and stored in a list.
 
 from event_generator.generators.customer_generator import generate_customer
 
@@ -47,16 +16,25 @@ def generate_customers(total_customers : int):
         customer=generate_customer() 
         customers.append(customer)
     return customers 
+# ==========================================================
+# Account Collection Generation
+# ==========================================================
+# Generates banking accounts for existing customers.
+# Each customer owns between 1 and 3 accounts.
 
 from event_generator.generators.account_generator import generate_account
+import random
 
-def generate_accounts(customers: int): 
+def generate_accounts(customers): 
     """
     Generate a list of accounts
     """
 
     accounts=[]
     for customer in customers:
-        account=generate_account(customer.customer_id) 
-        accounts.append(account)
+         # Randomly assign 1–3 accounts to each customer
+        number_of_accounts=random.randint(1,3)
+        for _ in range(number_of_accounts):
+            account=generate_account(customer.customer_id) 
+            accounts.append(account)
     return accounts 
--- event_generator/run.py
+++ event_generator/run.py
@@ -5,16 +5,16 @@ def main():
     customers= generate_customers(20000)   
     accounts=generate_accounts(customers)
 
-    # print(f"Customers: {len(customers)}")
-    # print(f"Accounts: {len(accounts)}")
+    print(f"Customers: {len(customers)}")
+    print(f"Accounts: {len(accounts)}")
 
-    print("\nFirst 5 Customers")
-    for customer in customers[:5]:
-        print(customer)
+    # print("\nFirst 5 Customers")
+    # for customer in customers[:5]:
+    #     print(customer)
 
-    print("\nFirst 5 Accounts")
-    for account in accounts[:5]:
-        print(account)
+    # print("\nFirst 5 Accounts")
+    # for account in accounts[:5]:
+    #     print(account)
 
 if __name__=="__main__":
     main()
