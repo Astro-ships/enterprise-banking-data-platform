410fbbc2570715aeee2a85fcc0779b2c40ef3c57
Astro-ships <adnankhanr6@gmail.com>
2026-08-04 00:00:24 +0500

    Fixed incorrect dataset mappings in 'raw_merchants'  RAW ingestion

--- sql/raw/raw_merchants.sql
+++ sql/raw/raw_merchants.sql
@@ -9,13 +9,13 @@ USE SCHEMA RAW;
 -- Create table using Snowflake Variant Feature
 -- ============================================
 
-CREATE TABLE IF NOT EXISTS RAW_MERCHANTS(
+CREATE OR REPLACE TABLE RAW_MERCHANTS(
     RAW_RECORD VARIANT
 );
 
 -- ============================
 -- Load data into merchants 
 -- ==========================
-COPY INTO RAW_CUSTOMERS  
+COPY INTO RAW_MERCHANTS  
 FROM @bank_stage/merchants.ndjson.gz
 FILE_FORMAT=json_format;
