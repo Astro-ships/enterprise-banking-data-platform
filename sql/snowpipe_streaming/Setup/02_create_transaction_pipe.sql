cc7bec3ab7ca45492d760843d29930021071438c
Muhammad Adnan <your_email@example.com>
2026-08-21 12:28:03 +0500

    (feat): Added Snowpipe Streaming Setup

---
+++ sql/snowpipe_streaming/Setup/02_create_transaction_pipe.sql
@@ -0,0 +1,30 @@
+-- ==============================
+-- Snowflake Session
+-- ==============================
+ 
+ USE ROLE ACCOUNTADMIN;
+ USE WAREHOUSE compute_wh;
+ USE DATABASE BANKING;
+ USE SCHEMA RAW_STREAMING;
+ -- =========================================
+ -- Confighure Snowflake Snowpipe Session
+ -- ========================================
+
+CREATE OR REPLACE PIPE BANKING.RAW_STREAMING.TRANSACTION_PIPE
+AS
+COPY INTO BANKING.RAW_STREAMING.TRANSACTIONS
+    (PAYLOAD, INGESTED_AT)
+FROM (
+    SELECT
+        $1,
+        CURRENT_TIMESTAMP()
+    FROM TABLE(
+        DATA_SOURCE(TYPE => 'STREAMING')
+    )
+);
+
+-- ======================
+-- Verify snowpipe 
+-- ======================
+
+DESC PIPE RAW_STREAMING.TRANSACTION_PIPE;
